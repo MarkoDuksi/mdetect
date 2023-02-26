a4d7d477d0a429b0ffb3bde0afacf7bf0b338149
MarkoDuksi <dookshi@gmail.com>
2023-02-26 10:02:57 +0100

    +inplace_downscale

--- main.cpp
+++ main.cpp
@@ -16,13 +16,18 @@ namespace cimg = cimg_library;
 #define HEIGHT_384 384
 #define SIZE_512x384 196608
 
+#define WIDTH_256 256
+#define HEIGHT_192 192
+#define SIZE_256x192 49152
+
 #define DEPTH_1 1
 #define CHANNELS_3 3
 #define CHANNELS_1 1
 
-void update_diff(uint8_t* background, uint8_t* img_curr, uint8_t* img_diff) {
-    for (size_t i = 0; i < SIZE_1024x768; ++i) {
-        img_diff[i] = std::abs(img_curr[i] - background[i]);
+void update_diff(uint8_t* image1, uint8_t* image2, uint8_t* diff, size_t width, size_t height) {
+    size_t size = width * height;
+    for (size_t i = 0; i < size; ++i) {
+        diff[i] = std::abs(image1[i] - image2[i]);
     }
 }
 
@@ -30,14 +35,35 @@ void update_background(uint8_t*& background, uint8_t*& img_curr) {
     std::swap(background, img_curr);
 }
 
-int main() {
-    // multi-purpose memory block
-    uint8_t stack_memory_pool[3 * SIZE_1024x768] = { 0 };
-    uint8_t* background = stack_memory_pool;
-    uint8_t* img_curr = &stack_memory_pool[SIZE_1024x768];
-    uint8_t* img_diff = &stack_memory_pool[2 * SIZE_1024x768];
+void inplace_downscale(uint8_t* image, size_t width, size_t height, size_t factor) {
+    size_t src_idx = 0;
+    size_t dst_idx = 0;
+    size_t box_idx = 0;
+    uint16_t accumulator;
 
-    // filenames from ./captures into sorted std::vector<std::string>
+    for (size_t src_row = 0; src_row < height; src_row += factor) {
+        for (size_t src_col = 0; src_col < width; src_col += factor) {
+            src_idx = src_row * width + src_col;
+            accumulator = 0;
+            for (size_t box_row = 0; box_row < factor; ++box_row) {
+                for (size_t box_col = 0; box_col < factor; ++box_col) {
+                    box_idx = src_idx + box_row * width + box_col;
+                    accumulator += image[box_idx];
+                }
+            }
+            image[dst_idx++] = accumulator / (factor * factor);
+        }
+    }
+}
+
+int main() {
+    // multi-purpose stack memory
+    uint8_t memory_pool[3 * SIZE_1024x768] = { 0 };
+    uint8_t* background = memory_pool;
+    uint8_t* img_curr = &memory_pool[SIZE_1024x768];
+    uint8_t* img_diff = &memory_pool[2 * SIZE_1024x768];
+
+    // filenames from 'captures/' into sorted std::vector<std::string>
     std::vector<std::string> dir_content;
     for (const auto& entry : std::filesystem::directory_iterator("./captures")) {
         dir_content.push_back(entry.path().string());
@@ -45,8 +71,8 @@ int main() {
     std::sort(dir_content.begin(), dir_content.end());
 
     // image containers and main diplay
-    cimg::CImg<uint8_t> img_rgb(WIDTH_1024, HEIGHT_768, DEPTH_1, CHANNELS_3);
-    cimg::CImg<uint8_t> img_greyscale(WIDTH_1024, HEIGHT_768, DEPTH_1, CHANNELS_1);
+    cimg::CImg<uint8_t> img_rgb;
+    cimg::CImg<uint8_t> img_greyscale;
     cimg::CImgDisplay disp;
 
     for (const auto& filename : dir_content) {
@@ -55,61 +81,14 @@ int main() {
         // copy only the green channel
         memcpy(img_curr, img_rgb._data + SIZE_1024x768, SIZE_1024x768);
 
-        update_diff(background, img_curr, img_diff);
-
-        img_greyscale.assign(img_diff, WIDTH_1024, HEIGHT_768);
-        disp = img_greyscale;
-
+        inplace_downscale(img_curr, WIDTH_1024, HEIGHT_768, 4);
+        update_diff(background, img_curr, img_diff, WIDTH_256, HEIGHT_192);
         update_background(background, img_curr);
+
+        img_greyscale.assign(img_diff, WIDTH_256, HEIGHT_192);
+        disp = img_greyscale;
         // disp.wait(100);
     }
 
     return 0;
 }
-
-// int main() {
-//     std::vector<std::string> dir_content;
-//     for (const auto& entry : std::filesystem::directory_iterator("./captures")) {
-//         dir_content.push_back(entry.path().string());
-//     }
-//     std::sort(dir_content.begin(), dir_content.end());
-
-//     cimg::CImgDisplay disp;
-
-//     cimg::CImg<uint8_t> img_rgb(dir_content[0].c_str());
-
-//     size_t width = img_rgb.width();
-//     size_t height = img_rgb.height();
-//     size_t size = width * height;
-
-//     cimg::CImg<uint8_t> img_greyscale(width, height);
-
-//     uint8_t* img0 = new uint8_t[size];
-//     uint8_t* img1 = new uint8_t[size];
-//     uint8_t* img_diff = new uint8_t[size];
-//     uint8_t* imgs[] = {img0, img1};
-
-//     size_t prev = 0;
-//     size_t curr = 1;
-
-//     memcpy(imgs[prev], img_rgb._data + size, size);
-
-//     for (auto path_string_it = dir_content.begin() + 1; path_string_it != dir_content.end(); ++path_string_it) {
-//         img_rgb.assign(path_string_it->c_str());
-//         memcpy(imgs[curr], img_rgb._data + size, size);
-//         for (size_t i = 0; i < size; ++i) {
-//             img_diff[i] = std::abs(imgs[curr][i] - imgs[prev][i]);
-//         }
-//         img_greyscale.assign(img_diff, width, height);
-//         disp = img_greyscale;
-        
-//         std::swap(prev, curr);
-//         disp.wait(100);
-//     }
-
-//     delete img0;
-//     delete img1;
-//     delete img_diff;
-
-//     return 0;
-// }
