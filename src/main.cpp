a6980b20d11b2f87cd697b01122b01644b72cc8b
MarkoDuksi <dookshi@gmail.com>
2023-05-21 20:00:20 +0200

    remove unnecessary img_rgb2

--- src/main.cpp
+++ src/main.cpp
@@ -44,27 +44,24 @@ int main(int argc, char** argv) {
 
     auto input_paths = getInputImgPaths(input_dir);
 
-    cimg::CImg<uint8_t> img_rgb1;
-    cimg::CImg<uint8_t> img_rgb2;
+    cimg::CImg<uint8_t> img_rgb;
 
-    img_rgb1.assign(input_paths[0].c_str());
+    img_rgb.assign(input_paths[0].c_str());
 
-    Image full_size_ref_img(WIDTH_1024, HEIGHT_768, &img_rgb1._data[WIDTH_1024 * HEIGHT_768]);
+    Image full_size_ref_img(WIDTH_1024, HEIGHT_768, &img_rgb._data[WIDTH_1024 * HEIGHT_768]);
     StaticImage<WIDTH_128, HEIGHT_96> downscaled_ref_img;
     StaticImage<WIDTH_128, HEIGHT_96> downscaled_img;
 
     transform::downscale_8x8(full_size_ref_img, downscaled_ref_img);
     MotionDetector<WIDTH_128, HEIGHT_96> motion(downscaled_ref_img);
 
-    // int counter = 1;
     for (const auto& input_path : input_paths) {
-        // if (counter++ < 372) continue;
 
         std::cout << "processing image: " << input_path << std::endl;
 
-        img_rgb2.assign(input_path.c_str());
+        img_rgb.assign(input_path.c_str());
 
-        Image full_size_img(WIDTH_1024, HEIGHT_768, &img_rgb2._data[WIDTH_1024 * HEIGHT_768]);
+        Image full_size_img(WIDTH_1024, HEIGHT_768, &img_rgb._data[WIDTH_1024 * HEIGHT_768]);
         transform::downscale_8x8(full_size_img, downscaled_img);
         
         auto bboxes = motion.detect(downscaled_img);
@@ -72,12 +69,12 @@ int main(int argc, char** argv) {
         if (bboxes.size()) {
             // uint subframe_count = 1;
             for (auto bbox : bboxes) {
-                img_rgb2.draw_rectangle(8 * bbox.topleft_X(), 8 * bbox.topleft_Y(), 8 * (bbox.bottomright_X()) - 1, 8 * (bbox.bottomright_Y()) - 1, green, 1, ~0U);
-                // img_rgb2.get_crop(8 * bbox.topleft_X, 8 * bbox.topleft_Y, 8 * (bbox.bottomright_X) - 1, 8 * (bbox.bottomright_Y) - 1)
+                img_rgb.draw_rectangle(8 * bbox.topleft_X(), 8 * bbox.topleft_Y(), 8 * (bbox.bottomright_X()) - 1, 8 * (bbox.bottomright_Y()) - 1, green, 1, ~0U);
+                // img_rgb.get_crop(8 * bbox.topleft_X, 8 * bbox.topleft_Y, 8 * (bbox.bottomright_X) - 1, 8 * (bbox.bottomright_Y) - 1)
                 //        .rotate(-90)
                 //        .save((std::string(output_dir) + "/" + input_path.stem().c_str() + "_" + std::to_string(subframe_count++) + ".jpg").c_str());
             }
-            img_rgb2.rotate(-90).save((std::string(output_dir) + "/" + input_path.filename().c_str()).c_str());
+            img_rgb.rotate(-90).save((std::string(output_dir) + "/" + input_path.filename().c_str()).c_str());
         }
     }
 
